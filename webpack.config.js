ec43672fcc3016d08d9906164c8a1e3efc547a08
zidiks <zidiks228@gmail.com>
2022-02-15 12:52:13 +0300

    init: webpack + scss + ts project

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: __dirname + '/src/app.ts',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        library: "modules"
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['./dist'] }
+        }),
+        new HtmlWebpackPlugin({
+            hash: true,
+            title: 'portfolio-webpack',
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{
+                    loader: 'html-loader'
+                }]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+}
